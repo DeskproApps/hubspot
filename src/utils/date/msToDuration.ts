78d73ee3519a87c2ce703287ace4a1d8b04b8d96
tidbit1 <194808961+tidbit1@users.noreply.github.com>
2025-01-22 11:09:25 +0000

    update FontAwesome dependencies and improve type handling in msToDuration function

--- src/utils/date/msToDuration.ts
+++ src/utils/date/msToDuration.ts
@@ -7,9 +7,9 @@ const msToDuration = (duration?: number): string => {
     let minutes = Math.floor((duration / (1000 * 60)) % 60) as string|number;
     let hours = Math.floor((duration / (1000 * 60 * 60)) % 24) as string|number;
 
-    hours = (hours < 10) ? "0" + hours : hours;
-    minutes = (minutes < 10) ? "0" + minutes : minutes;
-    seconds = (seconds < 10) ? "0" + seconds : seconds;
+    hours = (Number(hours) < 10) ? "0" + hours : hours.toString();
+    minutes = (Number(minutes) < 10) ? "0" + minutes : minutes.toString();
+    seconds = (Number(seconds) < 10) ? "0" + seconds : seconds.toString();
 
     return hours + ":" + minutes + ":" + seconds;
 }
