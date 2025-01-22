ef88cbc287755c66244f975f10d68a429ac43afa
tidbit1 <194808961+tidbit1@users.noreply.github.com>
2025-01-22 14:39:07 +0000

    refactor msToDuration function to improve parameter naming and simplify return statement

--- src/utils/date/msToDuration.ts
+++ src/utils/date/msToDuration.ts
@@ -1,17 +1,13 @@
-const msToDuration = (duration?: number): string => {
-    if (!duration) {
-        return "0";
-    }
+const msToDuration = (milliseconds?: number): string => {
+    if (!milliseconds) {
+        return '0';
+    };
 
-    let seconds = Math.floor((duration / 1000) % 60) as string|number;
-    let minutes = Math.floor((duration / (1000 * 60)) % 60) as string|number;
-    let hours = Math.floor((duration / (1000 * 60 * 60)) % 24) as string|number;
+    const seconds = Math.floor((milliseconds / 1000) % 60);
+    const minutes = Math.floor((milliseconds / (1000 * 60)) % 60);
+    const hours = Math.floor((milliseconds / (1000 * 60 * 60)) % 24);
 
-    hours = (Number(hours) < 10) ? '0' + hours : hours.toString();
-    minutes = (Number(minutes) < 10) ? '0' + minutes : minutes.toString();
-    seconds = (Number(seconds) < 10) ? '0' + seconds : seconds.toString();
-
-    return hours + ":" + minutes + ":" + seconds;
-}
+    return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+};
 
 export { msToDuration };
