5010c5ad9427ce4b7e324cbee9ea2aea356ac95d
OJ Abba <phavorabba@gmail.com>
2025-03-14 12:41:48 +0000

    [Bug] Fix runaway polling bug in `useLogin` (SC-183520)

--- src/pages/login/useLogin.ts
+++ src/pages/login/useLogin.ts
@@ -3,7 +3,7 @@ import { ContextData, Settings } from "../../types";
 import { createSearchParams, useNavigate } from "react-router-dom";
 import { getAccessToken } from "../../services/hubspot/";
 import { getEntityContactList } from "../../services/entityAssociation";
-import { OAuth2Result, useDeskproLatestAppContext, useInitialisedDeskproAppClient } from "@deskpro/app-sdk";
+import { IOAuth2, OAuth2Result, useDeskproLatestAppContext, useInitialisedDeskproAppClient } from "@deskpro/app-sdk";
 import { placeholders } from "../../services/hubspot/constants";
 import { tryToLinkAutomatically } from "../../utils";
 import { useCallback, useState } from "react";
@@ -20,6 +20,9 @@ export default function useLogin(): UseLogin {
     const [authUrl, setAuthUrl] = useState<string | null>(null)
     const [error, setError] = useState<null | string>(null)
     const [isLoading, setIsLoading] = useState(false)
+    const [isPolling, setIsPolling] = useState(false)
+    const [oauth2Context, setOAuth2Context] = useState<IOAuth2 | null>(null)
+
     const navigate = useNavigate()
 
     const { linkContactFn } = useLinkUnlinkNote();
@@ -55,7 +58,7 @@ export default function useLogin(): UseLogin {
             return
         }
 
-        const oauth2 = mode === "local" ?
+        const oauth2Response = mode === "local" ?
             await client.startOauth2Local(
                 ({ state, callbackUrl }) => {
                     return `https://app.hubspot.com/oauth/authorize?${createSearchParams([
@@ -69,7 +72,7 @@ export default function useLogin(): UseLogin {
                 /\bcode=(?<code>[^&#]+)/,
                 async (code: string): Promise<OAuth2Result> => {
                     // Extract the callback URL from the authorization URL
-                    const url = new URL(oauth2.authorizationUrl);
+                    const url = new URL(oauth2Response.authorizationUrl);
                     const redirectUri = url.searchParams.get("redirect_uri");
 
                     if (!redirectUri) {
@@ -84,38 +87,57 @@ export default function useLogin(): UseLogin {
             // Global Proxy Service
             : await client.startOauth2Global("0c24a533-90fe-4f5d-8882-ada7e3bfeed1");
 
-        setAuthUrl(oauth2.authorizationUrl)
-        setIsLoading(false)
+        setAuthUrl(oauth2Response.authorizationUrl)
+        setOAuth2Context(oauth2Response)
 
-        try {
-            const result = await oauth2.poll()
-
-            await client.setUserState(placeholders.OAUTH2_ACCESS_TOKEN_PATH, result.data.access_token, { backend: true })
-
-            if (result.data.refresh_token) {
-                await client.setUserState(placeholders.OAUTH2_REFRESH_TOKEN_PATH, result.data.refresh_token, { backend: true })
-            }
-
-            try {
-                await checkAuthService(client)
-            } catch {
-                throw new Error("Error authenticating user")
-            }
-
-            tryToLinkAutomatically(client, user, getContactInfo, linkContactFn)
-                .then(() => getEntityContactList(client, user.id))
-                .then((entityIds) => navigate(entityIds.length > 0 ? "/home" : "/link"))
-                .catch(() => { navigate("/link") });
-
-
-        } catch (error) {
-            setError(error instanceof Error ? error.message : 'Unknown error');
-            setIsLoading(false);
-        }
     }, [setAuthUrl, context?.settings.use_deskpro_saas])
+
+
+    useInitialisedDeskproAppClient((client) => {
+        if (!user || !oauth2Context) {
+            return
+        }
+
+        const startPolling = async () => {
+            try {
+                const result = await oauth2Context.poll()
+
+                await client.setUserState(placeholders.OAUTH2_ACCESS_TOKEN_PATH, result.data.access_token, { backend: true })
+
+                if (result.data.refresh_token) {
+                    await client.setUserState(placeholders.OAUTH2_REFRESH_TOKEN_PATH, result.data.refresh_token, { backend: true })
+                }
+
+                try {
+                    await checkAuthService(client)
+                } catch {
+                    throw new Error("Error authenticating user")
+                }
+
+                tryToLinkAutomatically(client, user, getContactInfo, linkContactFn)
+                    .then(() => getEntityContactList(client, user.id))
+                    .then((entityIds) => navigate(entityIds.length > 0 ? "/home" : "/link"))
+                    .catch(() => { navigate("/link") });
+
+
+            } catch (error) {
+                setError(error instanceof Error ? error.message : 'Unknown error');
+                setIsLoading(false);
+            } finally {
+                setIsLoading(false)
+                setIsPolling(false)
+            }
+        }
+
+        if (isPolling) {
+            void startPolling()
+        }
+    }, [isPolling, user, oauth2Context, navigate])
+
 
     const onSignIn = useCallback(() => {
         setIsLoading(true);
+        setIsPolling(true);
         window.open(authUrl ?? "", '_blank');
     }, [setIsLoading, authUrl]);
 
