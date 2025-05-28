1c00d2121ea4986cf09357f9ec8ecc94932e830b
Paul Hutchinson <paulhappyhutchinson@protonmail.ch>
2025-05-28 15:34:52 +0000

    Improve Error Reporting for Apps Sentry [sc-65330]

--- src/components/common/Error/ErrorFallback.tsx
+++ src/components/common/Error/ErrorFallback.tsx
@@ -1,4 +1,3 @@
-import { FC } from "react";
 import { match } from "ts-pattern";
 import { faRefresh } from "@fortawesome/free-solid-svg-icons";
 import { Button, Stack } from "@deskpro/deskpro-ui";
