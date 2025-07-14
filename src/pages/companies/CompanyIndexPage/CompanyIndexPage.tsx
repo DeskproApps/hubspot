c167c935c680f1a3c16d3adb3c7fe2af92cb33cd
tidbit1 <194808961+tidbit1@users.noreply.github.com>
2025-07-14 17:16:14 +0100

    redirect to view-company page if exists a linked company

--- src/pages/companies/CompanyIndexPage/CompanyIndexPage.tsx
+++ src/pages/companies/CompanyIndexPage/CompanyIndexPage.tsx
@@ -1,19 +1,42 @@
-import { LoadingSpinner, useDeskproLatestAppContext } from '@deskpro/app-sdk';
+import { LoadingSpinner, useDeskproLatestAppContext, useInitialisedDeskproAppClient } from '@deskpro/app-sdk';
 import { ContextData, Data, DeskproOrganisation, Settings } from '../../../types';
 import { useNavigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
+import { ErrorBlock } from '../../../components/common';
+import { getEntityCompanyList } from '../../../services/entityAssociation';
 
 function CompanyIndexPage() {
     const { context } = useDeskproLatestAppContext<ContextData, Settings>();
     const navigate = useNavigate();
     const [organisation, setOrganisation] = useState<DeskproOrganisation>();
+    const [error, setError] = useState('');
 
     useEffect(() => {
         setOrganisation(context?.data?.organisation);
     }, [context?.data?.organisation]);
 
+    useInitialisedDeskproAppClient(async client => {
+        if (!organisation) {
+            return;
+        };
+
+        try {
+            const linkedCompanies = await getEntityCompanyList(client, organisation.id);
+
+            if (linkedCompanies.length > 0) {
+                navigate(`/companies/${linkedCompanies[0]}`)
+            };
+        } catch (error) {
+            setError(error instanceof Error ? error.message : 'error loading company index');
+        };
+    }, [organisation]);
+
     if (organisation) {
         navigate('/companies/link');
+    };
+
+    if (error) {
+        return <ErrorBlock texts={[error]} />
     };
 
     return <LoadingSpinner />;
