a201486c40c5f01d110f553835ff2271fc3386e8
tidbit1 <194808961+tidbit1@users.noreply.github.com>
2025-07-22 16:57:23 +0100

    uninstall LoDash-ES--get in favour of native implementation

--- src/utils/filterEntities.ts
+++ src/utils/filterEntities.ts
@@ -1,13 +1,22 @@
-import { get } from "lodash-es";
 import { UseQueryResult } from "@tanstack/react-query";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const filterEntities = (entities: UseQueryResult[], path: string[] = ["data", "properties"]): any[] => {
     return entities
         ?.filter((entity) => (entity.isFetched && entity.isSuccess))
-        // re-introducing `lodash-es/get` as native implementation for `_.get`'s full robustness (dynamic paths, edge cases) proved overly complex, whereas `lodash-es` is tree-shakeable and efficient
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-        .map((entity) => get(entity, path))
+        .map(entity => {
+            let result: unknown = entity;
+
+            for (const key of path) {
+                if (typeof result !== "object" || result === null) {
+                    return;
+                };
+
+                result = (result as Record<string, unknown>)[key];
+            };
+
+            return result;
+        });
 }
 
 export { filterEntities };
