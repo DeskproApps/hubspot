fb5725a720e1a0e94e7502a587ee2dd7083c85e6
tidbit1 <194808961+tidbit1@users.noreply.github.com>
2025-07-22 16:10:08 +0100

    add comment explaining use of LoDash-ES--get

--- src/utils/filterEntities.ts
+++ src/utils/filterEntities.ts
@@ -5,6 +5,7 @@ import { UseQueryResult } from "@tanstack/react-query";
 const filterEntities = (entities: UseQueryResult[], path: string[] = ["data", "properties"]): any[] => {
     return entities
         ?.filter((entity) => (entity.isFetched && entity.isSuccess))
+        // re-introducing `lodash-es/get` as native implementation for `_.get`'s full robustness (dynamic paths, edge cases) proved overly complex, whereas `lodash-es` is tree-shakeable and efficient.
         // eslint-disable-next-line @typescript-eslint/no-unsafe-return
         .map((entity) => get(entity, path))
 }
